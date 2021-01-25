09a1fd176cc7936787c46ec9fc3a7f2c0a9674b1
Postoian Konstantin <hamelltonn@gmail.com>
2021-01-25 17:37:51 +0200

    new table and edit mobile table, in folder old version site

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+// var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
